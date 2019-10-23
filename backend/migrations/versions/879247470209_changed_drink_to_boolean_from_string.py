bed0b1589af367b2c75c6ba4c8a28c4535b57c41
Andreas Arnesson <andreas_osby@hotmail.com>
2019-10-23 19:56:44 +0200

    fixed database error on create

--- backend/migrations/versions/879247470209_changed_drink_to_boolean_from_string.py
+++ backend/migrations/versions/879247470209_changed_drink_to_boolean_from_string.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '879247470209'
-down_revision = '0045aa4bea13'
+down_revision = None
 branch_labels = None
 depends_on = None
 
